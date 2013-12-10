4d219706e2e5578b3cdca68ae6a4d6b3ad196ef4
Antoine Albertelli <antoinea101@gmail.com>
2013-12-10 22:14:39 +0100

    Removed useless plugins.

--- oral/main.js
+++ oral/main.js
@@ -35,10 +35,6 @@ Reveal.initialize({
     // Optional libraries used to extend on reveal.js
     dependencies: [
     { src: 'reveal/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-    { src: 'reveal/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-    { src: 'reveal/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-    { src: 'reveal/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-    { src: 'reveal/plugin/math/math.js', async: true }
 
     ]
 });
