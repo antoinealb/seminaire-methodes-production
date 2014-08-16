b9d0466f7e2270f81713207a7a87746704e587dc
Antoine Albertelli <antoinea101@gmail.com>
2014-08-16 23:10:47 +0200

    Remove / move old files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dot -Tpdf -o images/lds_process.pdf images/lds_process.dot &&
-pdflatex rapport.tex &&
-bibtex rapport.aux &&
-pdflatex rapport.tex && 
-pdflatex rapport.tex &&
-xdg-open rapport.pdf  2> /dev/null
--- todo.md
+++
@@ -1,6 +0,0 @@
-Todo list
-=========
-
-* Expliquer le LDS
-* Refaire les illus. du 2-shot
-* Faire une conclusion
